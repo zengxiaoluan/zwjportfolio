b5f1485cb56401a29c561d4c275ab544b5c160e0
wenjun zhou <wenjunzhou@wenjundeMacBook-Pro.local>
2017-01-21 11:24:37 +0800

    test

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
-<?php 
+<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
+<?php
+    
 echo "hello worldhhhh1234";
  ?>
 <h1>dfdfdf</h1>
-<style>
-h1{
-color:red;
-}
-</style>
